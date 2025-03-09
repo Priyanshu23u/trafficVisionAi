7909e59cb755fcdfc9e7cdeac5393ebc9de03713
Priyanshu23u <upriyanshujnp@gmail.com>
2025-03-10 00:18:23 +0530

    updated project

--- simulation.py
+++ simulation.py
@@ -60,31 +60,30 @@ detectionTime = 5
 speeds = {'car':2.25, 'bus':1.8, 'truck':1.8, 'rickshaw':2, 'bike':2.5}  # average speeds of vehicles
 
 # Coordinates of start
-x = {'right':[0,0,0], 'down':[271,254,240], 'left':[1400,1400,1400], 'up':[200,210,225]}    
-y = {'right':[223,232,250], 'down':[0,0,0], 'left':[300,285,268], 'up':[800,800,800]}
+x = {'right':[0,0,0], 'down':[755,727,697], 'left':[1400,1400,1400], 'up':[602,627,657]}    
+y = {'right':[348,370,398], 'down':[0,0,0], 'left':[498,466,436], 'up':[800,800,800]}
 
 vehicles = {'right': {0:[], 1:[], 2:[], 'crossed':0}, 'down': {0:[], 1:[], 2:[], 'crossed':0}, 'left': {0:[], 1:[], 2:[], 'crossed':0}, 'up': {0:[], 1:[], 2:[], 'crossed':0}}
 vehicleTypes = {0:'car', 1:'bus', 2:'truck', 3:'rickshaw', 4:'bike'}
 directionNumbers = {0:'right', 1:'down', 2:'left', 3:'up'}
 
 # Coordinates of signal image, timer, and vehicle count
-'''Changes of the Singnal Coods'''
-signalCoods = [(590,340),(675,260),(770,430),(675,510)]
+signalCoods = [(530,230),(810,230),(810,570),(530,570)]
 signalTimerCoods = [(530,210),(810,210),(810,550),(530,550)]
 vehicleCountCoods = [(480,210),(880,210),(880,550),(480,550)]
 vehicleCountTexts = ["0", "0", "0", "0"]
 
 # Coordinates of stop lines
-stopLines = {'right': 210, 'down': 220, 'left': 270, 'up': 307}
-defaultStop = {'right': 200, 'down': 210, 'left': 280, 'up': 317}
-stops = {'right': [200,200,200], 'down': [320,320,320], 'left': [810,810,810], 'up': [545,545,545]}
+stopLines = {'right': 590, 'down': 330, 'left': 800, 'up': 535}
+defaultStop = {'right': 580, 'down': 320, 'left': 810, 'up': 545}
+stops = {'right': [580,580,580], 'down': [320,320,320], 'left': [810,810,810], 'up': [545,545,545]}
 
 mid = {'right': {'x':705, 'y':445}, 'down': {'x':695, 'y':450}, 'left': {'x':695, 'y':425}, 'up': {'x':695, 'y':400}}
 rotationAngle = 3
 
 # Gap between vehicles
-gap = 7    # stopping gap
-gap2 = 7   # moving gap
+gap = 15    # stopping gap
+gap2 = 15   # moving gap
 
 pygame.init()
 simulation = pygame.sprite.Group()
@@ -158,7 +157,7 @@ class Vehicle(pygame.sprite.Sprite):
 
     def render(self, screen):
         screen.blit(self.currentImage, (self.x, self.y))
-        
+
     def move(self):
         if(self.direction=='right'):
             if(self.crossed==0 and self.x+self.currentImage.get_rect().width>stopLines[self.direction]):   # if the image has crossed stop line now
@@ -288,9 +287,9 @@ def setTime():
 #       greenTime = defaultMinimum
 #    elif(greenTime>defaultMaximum):
 #       greenTime = defaultMaximum
-#     greenTime = len(vehicles[currentGreen][0])+len(vehicles[currentGreen][1])+len(vehicles[currentGreen][2])
-#     noOfVehicles = len(vehicles[directionNumbers[nextGreen]][1])+len(vehicles[directionNumbers[nextGreen]][2])-vehicles[directionNumbers[nextGreen]]['crossed']
-#     print("no. of vehicles = ",noOfVehicles)
+    # greenTime = len(vehicles[currentGreen][0])+len(vehicles[currentGreen][1])+len(vehicles[currentGreen][2])
+    # noOfVehicles = len(vehicles[directionNumbers[nextGreen]][1])+len(vehicles[directionNumbers[nextGreen]][2])-vehicles[directionNumbers[nextGreen]]['crossed']
+    # print("no. of vehicles = ",noOfVehicles)
     noOfCars, noOfBuses, noOfTrucks, noOfRickshaws, noOfBikes = 0,0,0,0,0
     for j in range(len(vehicles[directionNumbers[nextGreen]][0])):
         vehicle = vehicles[directionNumbers[nextGreen]][0][j]
@@ -408,7 +407,7 @@ def generateVehicles():
         elif(temp<a[3]):
             direction_number = 3
         Vehicle(lane_number, vehicleTypes[vehicle_type], direction_number, directionNumbers[direction_number], will_turn)
-        time.sleep(0.25)
+        time.sleep(0.75)
 
 def simulationTime():
     global timeElapsed, simTime
@@ -446,7 +445,7 @@ class Main:
     screenSize = (screenWidth, screenHeight)
 
     # Setting background image i.e. image of intersection
-    background = pygame.image.load('first.png')
+    background = pygame.image.load('images/mod_int.png')
 
     screen = pygame.display.set_mode(screenSize)
     pygame.display.set_caption("SIMULATION")
